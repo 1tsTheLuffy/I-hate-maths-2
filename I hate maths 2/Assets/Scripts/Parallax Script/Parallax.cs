a24a551c2cf55afc1a212707b4f36778f3f45612
1tsTheLuffy <58876026+1tsTheLuffy@users.noreply.github.com>
2020-02-29 18:45:49 +0530

    1st Commit..

---
+++ I hate maths 2/Assets/Scripts/Parallax Script/Parallax.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] float speed = .5f;
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector2.left * speed * Time.fixedDeltaTime);
+    }
+}
---
+++ I hate maths 2/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(PlayerController))]
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 movement;
+    private Vector2 mousePos;
+    [Header("Floats")]
+    [SerializeField] float x1 = 1f;
+    [SerializeField] float x2 = 1f;
+    [SerializeField] float y1 = 1f;
+    [SerializeField] float y2 = 1f;
+
+    Rigidbody2D rb;
+    Camera cam;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        cam = Camera.main;
+
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePos;
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, x1, x2), Mathf.Clamp(transform.position.y, y1, y2), transform.position.z);
+    }
+}
